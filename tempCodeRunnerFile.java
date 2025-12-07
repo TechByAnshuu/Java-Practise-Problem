7761ead59ab23dc715b3a3485e862f74c5a3eff3
Ansh Mishra <anshmishra14.14@gmail.com>
2025-12-07 19:54:12 +0530

    Added new feature and fixed a bug

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,27 +1,24 @@
-import java.util.Scanner;
+import Java.util.Scanner;
 
-public class GuessTheNumber {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int randomNum = (int)(Math.random() * 100) + 1;
-        int userGuess = 0;
-        int attempts = 0;
-
-        System.out.println("Welcome to 'Guess the Number'! Try to guess the number between 1 and 100.");
-
-        while (userGuess != randomNum) {
-            System.out.print("Enter your guess: ");
+public class GuessTheNumber{
+    public static void main(String  args[]){
+        Scanner sc= new Scanner(System.in);
+        int randomNum= (int)(Math.ramdom()*100)+1;
+        int userGuess=0;
+        int attempts= 0;
+        System.out.println("Welcome to Guess the Number ! Try to guss the number 1 to 100.);
+        While(userGuess = randomNum){
+            System.out.println("Ente ryour Guess: ");
             userGuess = sc.nextInt();
             attempts++;
-
-            if (userGuess < randomNum) {
-                System.out.println("Too low! Try again.");
-            } else if (userGuess > randomNum) {
-                System.out.println("Too high! Try again.");
-            } else {
-                System.out.println("Congratulations! You've guessed the number " + randomNum + " in " + attempts + " attempts.");
-            }
+            if(userguess > attempts){
+                System.out.println("Too High! Try Again")
+            }else if(userGuess < attempts){
+                System.out.println("Too Low ! Try Again" )
+            }else{
+                System.out.Println("Congratulations! you mangaes to gues the Correct Number in " + attempts"+);
+            }           
         }
+
     }
 }
