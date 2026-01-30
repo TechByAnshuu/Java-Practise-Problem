379fb14f5ac1ba2094e5fb31475a73502d7a910f
Ansh Mishra <anshmishra14.14@gmail.com>
2026-01-30 22:24:15 +0530

    practise problem added

--- Practice.java
+++ Practice.java
@@ -1,8 +1,21 @@
-class Practice {
+import java.util.Scanner;
+public class Practice {
     public static void main(String[] args) {
-        int a;
-        int b;
-        int +,
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the name: " );
+
+        int number = sc.nextInt();
+
+        System.out.println("entered: " + number);
+
+
+        sc.close();
+
+
+
+
+
 
 
     }
