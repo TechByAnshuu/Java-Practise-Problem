4d353935fd970dd23d89f3409a44a1891e95044d
Ansh Mishra <anshmishra14.14@gmail.com>
2026-02-11 23:24:57 +0530

    practise problem added

---
+++ src/main/java/Leetcode - 40.java
@@ -0,0 +1,129 @@
+
+// 3721. Longest Balanced Subarray II
+// Hard
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You are given an integer array nums.
+
+// A subarray is called balanced if the number of distinct even numbers in the subarray is equal to the number of distinct odd numbers.
+
+// Return the length of the longest balanced subarray.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,5,4,3]
+
+// Output: 4
+
+// Explanation:
+
+// The longest balanced subarray is [2, 5, 4, 3].
+// It has 2 distinct even numbers [2, 4] and 2 distinct odd numbers [5, 3]. Thus, the answer is 4.
+// Example 2:
+
+// Input: nums = [3,2,2,5,4]
+
+// Output: 5
+
+// Explanation:
+
+// The longest balanced subarray is [3, 2, 2, 5, 4].
+// It has 2 distinct even numbers [2, 4] and 2 distinct odd numbers [3, 5]. Thus, the answer is 5.
+// Example 3:
+
+// Input: nums = [1,2,3,2]
+
+// Output: 3
+
+// Explanation:
+
+// The longest balanced subarray is [2, 3, 2].
+// It has 1 distinct even number [2] and 1 distinct odd number [3]. Thus, the answer is 3.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// 1 <= nums[i] <= 105
+
+
+
+class Solution {
+    public int longestBalanced(String s) {
+        char[] cs = s.toCharArray();
+        int x = calc1(cs);
+        int y = Math.max(calc2(cs, 'a', 'b'), Math.max(calc2(cs, 'b', 'c'), calc2(cs, 'a', 'c')));
+        int z = calc3(cs);
+        return Math.max(x, Math.max(y, z));
+    }
+
+    private int calc1(char[] s) {
+        int res = 0;
+        int i = 0, n = s.length;
+        while (i < n) {
+            int j = i + 1;
+            while (j < n && s[j] == s[i]) {
+                j++;
+            }
+            res = Math.max(res, j - i);
+            i = j;
+        }
+        return res;
+    }
+
+    private int calc2(char[] s, char a, char b) {
+        int res = 0;
+        int i = 0, n = s.length;
+        while (i < n) {
+            while (i < n && s[i] != a && s[i] != b) {
+                i++;
+            }
+            Map<Integer, Integer> pos = new HashMap<>();
+            pos.put(0, i - 1);
+            int d = 0;
+            while (i < n && (s[i] == a || s[i] == b)) {
+                d += (s[i] == a) ? 1 : -1;
+                Integer prev = pos.get(d);
+                if (prev != null) {
+                    res = Math.max(res, i - prev);
+                } else {
+                    pos.put(d, i);
+                }
+                i++;
+            }
+        }
+        return res;
+    }
+
+    private int calc3(char[] s) {
+        Map<Long, Integer> pos = new HashMap<>();
+        pos.put(f(0, 0), -1);
+
+        int[] cnt = new int[3];
+        int res = 0;
+
+        for (int i = 0; i < s.length; i++) {
+            char c = s[i];
+            ++cnt[c - 'a'];
+            int x = cnt[0] - cnt[1];
+            int y = cnt[1] - cnt[2];
+            long k = f(x, y);
+
+            Integer prev = pos.get(k);
+            if (prev != null) {
+                res = Math.max(res, i - prev);
+            } else {
+                pos.put(k, i);
+            }
+        }
+        return res;
+    }
+
+    private long f(int x, int y) {
+        return (x + 100000) << 20 | (y + 100000);
+    }
+}
